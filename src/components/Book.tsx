dc8a132299959f6aed7b9fa4151978dfbe8be0d6
hayeong choi <hayeong28@naver.com>
2020-07-27 17:49:25 +0900

    add: write BookResType

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import styles from './Book.module.css';
 import { BookResType } from '../types';
+import { BookOutlined } from '@ant-design/icons';
 
 interface BookProps extends BookResType {}
 
 // [project] 컨테이너에 작성된 함수를 컴포넌트에서 이용했다.
 // [project] BookResType 의 응답 값을 이용하여, Book 컴포넌트를 완성했다.
 const Book: React.FC<BookProps> = () => {
-  return <div className={styles.book}>Book</div>;
+  return (
+    <div className={styles.book}>
+      <BookOutlined /> {'book.title'}
+    </div>
+  );
 };
 
 export default Book;
