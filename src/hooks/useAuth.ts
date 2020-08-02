bdf5522ef84bb2f8bb303e6ffc1745ae329376dc
Ha0 <hayeong28@naver.com>
2020-08-03 00:37:26 +0900

    add: make useAuth custom Hooks for maintainability

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,27 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { logout as logoutSaga } from '../redux/modules/auth';
+import { login as loginSaga } from '../redux/modules/auth';
+import { LoginReqType } from '../types';
+
+type useAuthRetType = {
+  login: ({ email, password }: LoginReqType) => void;
+  logout: () => void;
+};
+
+export function useAuth(): useAuthRetType {
+  const dispatch = useDispatch();
+
+  const login = useCallback(
+    ({ email, password }: LoginReqType) => {
+      dispatch(loginSaga({ email, password }));
+    },
+    [dispatch],
+  );
+
+  const logout = useCallback(() => {
+    dispatch(logoutSaga());
+  }, [dispatch]);
+
+  return { login, logout };
+}
