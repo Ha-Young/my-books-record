4e3460e9129bad80e57b99736270be7d95fce244
hychoi <hayeong28@naver.com>
2020-08-09 17:45:38 +0900

    Updates

---
+++ precache-manifest.40d747cd909d56e39fa0d2ca7b4a2bc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2623c9322f85a0e232ec76dd09f38c98",
+    "url": "/my-books-record/index.html"
+  },
+  {
+    "revision": "b87e736cb2abd87d69c4",
+    "url": "/my-books-record/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "288b270dafa6a23a8505",
+    "url": "/my-books-record/static/css/main.62a2e127.chunk.css"
+  },
+  {
+    "revision": "b87e736cb2abd87d69c4",
+    "url": "/my-books-record/static/js/2.4103c4d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-books-record/static/js/2.4103c4d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288b270dafa6a23a8505",
+    "url": "/my-books-record/static/js/main.e1c10751.chunk.js"
+  },
+  {
+    "revision": "40e03e10f2939a99f66d",
+    "url": "/my-books-record/static/js/runtime-main.e4c102d8.js"
+  }
+]);
